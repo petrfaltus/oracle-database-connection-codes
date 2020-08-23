145726fdade375c8ee491a8a52544aa0b7e0ff8d
Petr Faltus <petrfaltus@petrfaltus.net>
2020-08-23 10:50:37 +0200

    C# - connect to the database

--- csharp/01-compile.cmd
+++ csharp/01-compile.cmd
@@ -5,4 +5,4 @@ set EXECUTABLE=bin\%SOURCE%.exe
 set ICON=ico\database.ico
 set DOTNET_64_HOME=C:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319
 
-"%DOTNET_64_HOME%\csc.exe" /win32icon:%ICON% /out:%EXECUTABLE% %SOURCE%.cs
+"%DOTNET_64_HOME%\csc.exe" /win32icon:%ICON% /out:%EXECUTABLE% /reference:bin\Oracle.ManagedDataAccess.dll %SOURCE%.cs
--- csharp/OracleDBclient.cs
+++ csharp/OracleDBclient.cs
@@ -1,10 +1,41 @@
 using System;
+using Oracle.ManagedDataAccess.Client;
 
 public class OracleDBclient
 {
+    private const string db_host = "localhost";
+    private const int db_port = 1521;
+    private const string db_srvname = "ORCLCDB.localdomain";
+    private const string db_username = "testuser";
+    private const string db_password = "T3stUs3r!";
+
     public static void Main(string[] args)
     {
-        Console.WriteLine("OK");
+        // Build the connection string
+        OracleConnectionStringBuilder connBuilder = new OracleConnectionStringBuilder();
+        connBuilder.DataSource = String.Format("{0}:{1}/{2}", db_host, db_port, db_srvname);
+        connBuilder.UserID = db_username;
+        connBuilder.Password = db_password;
+
+        try
+        {
+            using (OracleConnection conn = new OracleConnection(connBuilder.ConnectionString))
+            {
+                // Connect the database
+                conn.Open();
+
+                Console.WriteLine("Data source: {0}", conn.DataSource);
+                Console.WriteLine("Server version: {0}", conn.ServerVersion);
+                Console.WriteLine("Connection timeout: {0}", conn.ConnectionTimeout);
+                Console.WriteLine("State: {0}", conn.State);
+                Console.WriteLine();
+
+            }
+        }
+        catch (OracleException oex)
+        {
+            Console.WriteLine("Oracle database error: {0}", oex.Message);
+        }
 
     }
 }
