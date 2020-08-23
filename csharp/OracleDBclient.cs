a7f8e2c5fc9e19b2babc8a7d9d35cdb362eb583e
Petr Faltus <petrfaltus@petrfaltus.net>
2020-08-23 11:03:51 +0200

    C# - the full SELECT statement

--- csharp/OracleDBclient.cs
+++ csharp/OracleDBclient.cs
@@ -9,6 +9,8 @@ public class OracleDBclient
     private const string db_username = "testuser";
     private const string db_password = "T3stUs3r!";
 
+    private const string db_table = "cars";
+
     public static void Main(string[] args)
     {
         // Build the connection string
@@ -30,6 +32,62 @@ public class OracleDBclient
                 Console.WriteLine("State: {0}", conn.State);
                 Console.WriteLine();
 
+                // Full SELECT statement
+                string sql1 = String.Format("select * from {0}", db_table);
+                using (var cmd = new OracleCommand(sql1, conn))
+                    using (OracleDataReader reader = cmd.ExecuteReader())
+                    {
+                        int columns = reader.FieldCount;
+                        Console.WriteLine("Total columns: {0}", columns);
+
+                        for (int ii = 0; ii < columns; ii++)
+                        {
+                            Console.WriteLine(" - {0} {1}", reader.GetName(ii), reader.GetDataTypeName(ii));
+                        }
+
+                        int number = 0;
+                        while (reader.Read())
+                        {
+                            number++;
+                            Console.Write(number);
+
+                            for (int ii = 0; ii < columns; ii++)
+                            {
+                                string type = reader.GetDataTypeName(ii);
+
+                                string value = "?";
+                                if (!reader.IsDBNull(ii))
+                                {
+                                    if (type.EndsWith("Varchar2"))
+                                    {
+                                        value = reader.GetString(ii);
+                                    }
+                                    else if (type.Equals("TimeStamp"))
+                                    {
+                                        value = reader.GetDateTime(ii).ToString();
+                                    }
+                                    else if (type.Equals("Int16"))
+                                    {
+                                        value = reader.GetInt16(ii).ToString();
+                                    }
+                                    else if (type.Equals("Int32"))
+                                    {
+                                        value = reader.GetInt32(ii).ToString();
+                                    }
+                                }
+                                else
+                                {
+                                    value = "(null)";
+                                }
+
+                                Console.Write(" '{0}'", value);
+                            }
+
+                            Console.WriteLine();
+                        }
+                    }
+
+                Console.WriteLine();
             }
         }
         catch (OracleException oex)
