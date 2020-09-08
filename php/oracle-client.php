062cf194c56766b724e994a05d51ec595117bfc8
Petr Faltus <petrfaltus@petrfaltus.net>
2020-09-08 10:43:51 +0200

    PHP - CALL package procedure statement

--- php/oracle-client.php
+++ php/oracle-client.php
@@ -36,6 +36,16 @@ $db_column_value = 1;
 $db_factorial_variable = ":n";
 $db_factorial_value = 4;
 
+$db_add_and_subtract_a_variable = ":a";
+$db_add_and_subtract_a_value = 12;
+$db_add_and_subtract_b_variable = ":b";
+$db_add_and_subtract_b_value = 5;
+
+$db_add_and_subtract_x_variable = ":x";
+$db_add_and_subtract_x_value = -1;
+$db_add_and_subtract_y_variable = ":y";
+$db_add_and_subtract_y_value = -1;
+
 $availableDrivers = PDO::getAvailableDrivers();
 
 echo "Available PDO drivers ";
@@ -115,8 +125,28 @@ try
      print_r($stm3->errorInfo());
    else
      print_r($lines2);
+   echo PHP_EOL;
 
    $stm3 = null;
+
+   // CALL package procedure statement
+   $stm4 = $conn->prepare("call calculator.add_and_subtract(".$db_add_and_subtract_a_variable.", ".$db_add_and_subtract_b_variable.", ".$db_add_and_subtract_x_variable.", ".$db_add_and_subtract_y_variable.")");
+   $stm4->bindParam($db_add_and_subtract_a_variable, $db_add_and_subtract_a_value, PDO::PARAM_INT);
+   $stm4->bindParam($db_add_and_subtract_b_variable, $db_add_and_subtract_b_value, PDO::PARAM_INT);
+   $stm4->bindParam($db_add_and_subtract_x_variable, $db_add_and_subtract_x_value, PDO::PARAM_INT | PDO::PARAM_INPUT_OUTPUT, 20);
+   $stm4->bindParam($db_add_and_subtract_y_variable, $db_add_and_subtract_y_value, PDO::PARAM_INT | PDO::PARAM_INPUT_OUTPUT, 20);
+   $stm4->execute();
+
+   $stm4error = $stm4->errorInfo();
+   if ((isset($stm4error[0])) and ($stm4error[0] === "00000"))
+     {
+      echo "X: ".$db_add_and_subtract_x_value.PHP_EOL;
+      echo "Y: ".$db_add_and_subtract_y_value.PHP_EOL;
+     }
+   else
+     print_r($stm4error);
+
+   $stm4 = null;
 
    // Disconnect the database
    $conn = null;
