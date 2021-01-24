2d1c7abf8a855d1c7e74f1e9877c13c833016758
Petr Faltus <petrfaltus@petrfaltus.net>
2021-01-24 14:48:53 +0100

    Use the environment variable for the output directory

--- csharp/01-compile.cmd
+++ csharp/01-compile.cmd
@@ -7,7 +7,7 @@ set EXECUTABLE=%OUT%\%SOURCE%.exe
 set ICON=ico\database.ico
 set DOTNET_HOME=C:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319
 
-set DLL=bin\Oracle.ManagedDataAccess.dll
+set DLL=%OUT%\Oracle.ManagedDataAccess.dll
 
 mkdir %OUT%
 "%DOTNET_HOME%\csc.exe" /win32icon:%ICON% /out:%EXECUTABLE% /reference:%DLL% %SOURCE%.cs
